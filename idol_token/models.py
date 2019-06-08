2a39a5e16fc405d12bf388501fe30a117fd25990
松岡主馬 <>
2019-06-08 20:09:58 +0900

    fix conflict

--- idol_token/models.py
+++ idol_token/models.py
@@ -4,13 +4,8 @@ from cloudinary.models import CloudinaryField
 # Create your models here.
 class Idol(models.Model):
     name = models.CharField(max_length = 50)
-<<<<<<< HEAD
-    image = models.ImageField(null = True, blank = True)
     address = models.CharField(max_length = 255)
-=======
     image = CloudinaryField('image', blank=True, null=True,)
-    address = models.CharField(max_length=255)
->>>>>>> 7a8e83fbff2978c8c2c48cab236dbf78d6fb830d
     construct_id = models.CharField(max_length = 250, null=True, blank=True)
 
     def __str__(self):
@@ -21,11 +16,7 @@ class Idol_Item(models.Model):
     idol = models.ForeignKey(Idol, on_delete = models.CASCADE)
     contents = models.CharField(max_length = 250, null = True, blank = True)
     price = models.IntegerField(default = 0)
-<<<<<<< HEAD
-    image = models.ImageField(null = True, blank = True)
-=======
     image = CloudinaryField('image', blank=True, null=True,)
->>>>>>> 7a8e83fbff2978c8c2c48cab236dbf78d6fb830d
     total_issue_value = models.IntegerField(default = 0)
 
     def __str__(self):
--- idol_token/views.py
+++ idol_token/views.py
@@ -6,25 +6,19 @@ import json
 
 # Create your views here.
 def top(request):
-    data = list(Idol.objects.all()[:4].values("name", "image", "address", "construct_id"))
+    data = list(Idol.objects.all()[:4].values("id", "name", "image", "address", "construct_id"))
     return JsonResponse({"data": data})
 
 # Show the "Idol" page
 def idol(request, idol_id):
-    data = list(Idol.objects.filter(id = idol_id).values("name", "image", "address", "construct_id"))
+    data = list(Idol.objects.filter(id = idol_id).values("id", "name", "image", "address", "construct_id"))
     return JsonResponse({"data": data})
 
 # Show all Idol data
 def all_idol(request):
-    data = list(Idol.objects.all().values("name", "image", "address", "construct_id"))
+    data = list(Idol.objects.all().values("id", "name", "image", "address", "construct_id"))
     return JsonResponse({"data": data})
 
-<<<<<<< HEAD
-# For Idol page
-def idol_page(request):
-    if request.method != "POST":
-        return redirect(to = "/idol_token")
-=======
 def get_idol(request, pk=None):
     idol = Idol.objects.get(address=pk).values('id', 'name', 'image', 'address')
     data = {
@@ -50,4 +44,3 @@ def register_item(request):
         params = request.POST
         title = params.get('title')
         address = params.get('address')
->>>>>>> 7a8e83fbff2978c8c2c48cab236dbf78d6fb830d
