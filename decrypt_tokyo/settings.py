325ac8743c005e8f4261dd565159a352d643ac1f
tenkanooodorobou-ishikawagoemon <42778436+tenkanooodorobou-ishikawagoemon@users.noreply.github.com>
2019-06-08 13:56:05 +0900

    Add files via upload

--- decrypt_tokyo/settings.py
+++ decrypt_tokyo/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'decrypt_tokyo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'idol_token',
+        'USER': 'root',
+
     }
 }
 
