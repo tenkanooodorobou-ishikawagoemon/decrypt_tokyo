649fb3c57dd99cef445b0bcdc26d2a48e3608ff0
Toshiaki <toshi@issho.io>
2019-06-08 15:35:37 +0900

    tokenURIの追加

--- contract/contracts/IdolToken.sol
+++ contract/contracts/IdolToken.sol
@@ -1,9 +1,16 @@
 pragma solidity 0.5.8;
-import "openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol";
+import "https://github.com/ProjectOpenSea/opensea-creatures/contracts/Strings.sol";
+
+// import "openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol";
+// import "./Strings.sol";
 
 contract IdolToken is ERC721Full {
-    constructor(string memory _name, string memory _symbol) ERC721Full(_name, _symbol) public {}
+    
+    using Strings for string;
 
+    constructor(string memory _name, string memory _symbol) ERC721Full(_name, _symbol) public {}
+    
     struct Voice {
         address owner;
         uint256 totalSupply;
@@ -14,13 +21,14 @@ contract IdolToken is ERC721Full {
     mapping (address => uint256[]) public ownedVoices;
     mapping (uint256 => Voice) public voices; // voiceId : Voice
     mapping (uint256 => uint256) public voiceIds; // tokenId : voiceId
+    
+    string baseUrl = "https://sample.com/voice/";
 
     // アイドルがトークンの設定をする
     // 音声ファイルID、最大発行料、金額
     function releaseVoice(uint256 _voiceId, uint256 _totalSupply, uint256 _price) public {
         Voice memory newVoice = Voice({
             owner: msg.sender,
-            voiceId: _voiceId,
             totalSupply: _totalSupply,
             issuedNum: 0,
             price: _price
@@ -49,6 +57,17 @@ contract IdolToken is ERC721Full {
     }
 
     // ユーザーが購入する（発行）
-    function mintVoice(uint256 _voiceId) public {
-    }
+    function mintVoice(uint256 _voiceId) public payable{
+        
+       Voice storage newVoice = voices[_voiceId];
+       require(msg.value == newVoice.price);
+       require(newVoice.issuedNum < newVoice.totalSupply);
+       uint256 tokenId = totalSupply();
+       _mint(msg.sender, tokenId);
+       voiceIds[tokenId] = _voiceId;
+       newVoice.issuedNum++;
+       
+      string memory uri = Strings.strConcat(baseUrl, Strings.uint2str(tokenId));
+      _setTokenURI(tokenId, uri);
+   }
 }
