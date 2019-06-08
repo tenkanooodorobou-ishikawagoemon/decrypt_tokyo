a69091c541af15347d4d78ac27d3dcf16b585bf8
tsubasa onoue <>
2019-06-08 23:01:12 +0900

    fix: conflict

--- idol_token/urls.py
+++ idol_token/urls.py
@@ -8,10 +8,9 @@ urlpatterns = [
     path("all_idol/", all_idol, name = "all_idol"),
     # To show the "Idol" page
     path("<int:idol_id>/", idol, name = "idol"),
-    path("idol_page/", idol_page, name = "idol_page"),
+    # path("idol_page/", idol_page, name = "idol_page"),
     #=========TEST=============
-    path("test/", test, name = "test"),
-    path('index/', get_index, name='index')
+    # path("test/", test, name = "test"),
     path('index/', get_index, name='index'),
-    path('idol/', get_idol, name='idol'),
+    path('idol/', get_idol, name='idol_detail'),
 ]
