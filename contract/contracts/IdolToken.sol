0b46a4978c4561699e5de1037011baf4ac71e624
Toshiaki <toshi@issho.io>
2019-06-08 14:11:08 +0900

    コントラクトの追加

---
+++ contract/contracts/IdolToken.sol
@@ -0,0 +1,54 @@
+pragma solidity 0.5.8;
+import "openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol";
+
+contract IdolToken is ERC721Full {
+    constructor(string memory _name, string memory _symbol) ERC721Full(_name, _symbol) public {}
+
+    struct Voice {
+        address owner;
+        uint256 totalSupply;
+        uint256 issuedNum;
+        uint256 price;
+    }
+
+    mapping (address => uint256[]) public ownedVoices;
+    mapping (uint256 => Voice) public voices; // voiceId : Voice
+    mapping (uint256 => uint256) public voiceIds; // tokenId : voiceId
+
+    // アイドルがトークンの設定をする
+    // 音声ファイルID、最大発行料、金額
+    function releaseVoice(uint256 _voiceId, uint256 _totalSupply, uint256 _price) public {
+        Voice memory newVoice = Voice({
+            owner: msg.sender,
+            voiceId: _voiceId,
+            totalSupply: _totalSupply,
+            issuedNum: 0,
+            price: _price
+        });
+        voices[_voiceId] = newVoice;
+    }
+
+    function getVoice(uint256 _voiceId) public view returns(address, uint256, uint256, uint256) {
+        Voice memory newVoice = voices[_voiceId];
+        return ( newVoice.owner, newVoice.totalSupply, newVoice.issuedNum, newVoice.price);
+    }
+
+    // アイドルの音声IDに紐づくトークンの最大発行料
+    function getTotalSupply(uint256 _voiceId) public view returns(uint256) {
+        return voices[_voiceId].totalSupply;
+    }
+
+    // アイドルの音声IDに紐づくトークンのすでに発行されている数
+    function getIssuedNum(uint256 _voiceId) public view returns(uint256) {
+        return voices[_voiceId].issuedNum;
+    }
+
+    // アイドルの音声IDに紐づくトークンの価格
+    function getPrice(uint256 _voiceId) public view returns(uint256) {
+        return voices[_voiceId].price;
+    }
+
+    // ユーザーが購入する（発行）
+    function mintVoice(uint256 _voiceId) public {
+    }
+}
