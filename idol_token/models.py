dd7b676361d0a63ecbeefdd997ecf10789cd3988
tsubasa onoue <>
2019-06-08 18:43:59 +0900

    fix: model

--- idol_token/models.py
+++ idol_token/models.py
@@ -3,16 +3,19 @@ from django.db import models
 # Create your models here.
 class Idol(models.Model):
     name = models.CharField(max_length = 50)
-    image = models.ImageField()
-    construct_id = models.CharField(max_length = 250)
+    image = models.ImageField(null=True, blank=True)
+    address = models.CharField(max_length=255)
+    construct_id = models.CharField(max_length = 250, null=True, blank=True)
 
     def __str__(self):
         return "{0}".format(self.name)
 
 class Idol_Item(models.Model):
+    title = models.CharField(max_length=50)
     idol = models.ForeignKey(Idol, on_delete = models.CASCADE)
-    contents = models.CharField(max_length = 250)
+    contents = models.CharField(max_length=250, null=True, blank=True)
     price = models.IntegerField(default = 0)
+    image = models.ImageField(null=True, blank=True)
     total_issue_value = models.IntegerField(default = 0)
 
     def __str__(self):
